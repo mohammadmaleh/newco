0b2bcdb02e127719ee53b549c5dbb9c64126169e
mohammad maleh <mohammad.maleh@gmail.com>
2017-10-05 01:36:37 +0300

    deploy

--- app/api/fileTypeAPI.js
+++ app/api/fileTypeAPI.js
@@ -5,7 +5,7 @@ import axios from 'axios'
 export const getAllFileTypes = ()=>{
     return axios({
         method: 'get',
-        url:  'http://localhost:3000/api/fileType',
+        url:  '/api/fileType',
     });
 
 }
@@ -16,7 +16,7 @@ export const getAllFileTypes = ()=>{
 export const postFileTypes = (fileType)=>{
     return axios({
         method: 'post',
-        url:  'http://localhost:3000/api/fileType',
+        url:  '/api/fileType',
         data:fileType
     });
 
@@ -24,7 +24,7 @@ export const postFileTypes = (fileType)=>{
 export const patchFileTypes = (id,fileType)=>{
     return axios({
         method: 'patch',
-        url:  'http://localhost:3000/api/fileType/'+id,
+        url:  '/api/fileType/'+id,
         data:fileType
     });
 
@@ -33,14 +33,14 @@ export const patchFileTypes = (id,fileType)=>{
 export const deleteFileType = (id)=>{
     return axios({
         method: 'delete',
-        url:  'http://localhost:3000/api/fileType/'+id,
+        url:  '/api/fileType/'+id,
     });
 
 }
 export const availableFileTypes = ()=>{
     return axios({
         method: 'get',
-        url:  'http://localhost:3000/api/availableFileType/',
+        url:  '/api/availableFileType/',
     });
 
 }
